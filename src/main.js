55951e73258adc7d811745983686b6191c7c18a2
Lin0619 <applelin96@icloud.com>
2023-07-06 16:49:55 +0800

    bb

--- src/main.js
+++ src/main.js
@@ -33,6 +33,48 @@ function CallbackFunc(Error, Results){
     Results.forEach(data => console.log(data));
 }
 
-let sql = ``
+let sql = 
+    `CREATE TABLE IF NOT EXISTS Users(
+    ID TEXT PRIMARY KEY,
+    username TEXT,
+    Email TEXT,
+    Dob TEXT
+    );`
 
+let table = 'SELECT name FROM pragma_table_info("Users");'
+
+let x=`
+ALTER TABLE Users
+RENAME COLUMN Email TO Emali;
+`
+let y =`
+INSERT INTO Users(ID,username,Emali,Dob)
+VALUES("11011","Bob","bob.6666@gmail.com","2023-0555-06")
+`
+
+let z=`
+SELECT * FROM Users;
+`
+const a=`
+SELECT * FROM Users
+WHERE id = "111111";
+`
+const b =`
+    UPDATE Users
+    SET Emali = "sheeep"
+    WHERE id ="111111";
+    `
+
+// db.all(x,CallbackFunc);
+//db.all(y,CallbackFunc);
+//db.all(z,CallbackFunc);
+// db.all(a,CallbackFunc);
+//db.all(table,CallbackFunc);
 db.exec(sql, CallbackFunc);
+// db.all(b, CallbackFunc);
+
+let c=`
+DELETE FROM Users
+WHERE id ="111111";
+`
+db.all(c, CallbackFunc);
